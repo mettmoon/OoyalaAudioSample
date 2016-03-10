7a2a93bbc9efe3cee7e9b8e2022fbdd2851018ba
Peter <peter@olivestory.com>
2016-03-10 12:36:32 +0900

    Initial Commit

---
+++ ooyala/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  ooyala
+//
+//  Created by Moon Hayden on 3/10/16.
+//  Copyright © 2016 unkown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
